08e8c99bf69fb9e7abab59d25dfba4eb7f827530
Philip Lin <philipo227@hotmail.com>
2016-09-30 15:48:05 -0700

    edit models

--- models/characters.js
+++ models/characters.js
@@ -2,13 +2,13 @@ var
   mongoose = require('mongoose'),
 
   characterSchema = mongoose.Schema({
-    name: String,
+    name: {type: String, required: true},
     power: Array,
     sex: String,
     created: Number,
     universe: String,
     hero: Boolean
-  })
+  }, {timestamps: true})
 
 var Character = mongoose.model('Character', characterSchema)
 
